ccae062dae379bc1faf3abb2c09f349df6081225
adbata26 <adbata26@gmail.com>
2024-02-15 13:23:05 +0800

    UP TO DAY 4

---
+++ Day4/sample.js
@@ -0,0 +1,104 @@
+//variables
+// var - global scope
+// let - block scope
+// const -block scope/ unmutable
+
+// function sayHello() {
+//     var user = "Aaron";
+
+//     console.log("Hello, "+user);
+// }
+
+// var user = "Aaron";
+// console.log("Hello, "+user);
+
+// sayHello();
+
+//data types
+// String - "Friendric", "4874", "jay$123"
+// Number - -145, 486474878874, 45.2456465487
+// Boolean - true or false
+// Date - 01/23/2015
+// Undefined - missing value
+// Null - empty
+
+// var a = 10;
+// var b = 50;
+
+// var c = a + b;
+// console.log(c);
+
+
+//JS operators
+// arithmetic: +, -, *, /
+// logical: AND (&&), OR (||), NOT(!)
+
+// var x = true;
+// var y = false;
+
+// console.log(!x);
+
+// false && false = false;
+// false && true = false;
+// true && false = false;
+// true && true = true;9
+
+//comparison operators: <, >, ==, !=, <=, >=
+
+// = vs ==
+
+//Assignment operators: +=, -=, *=, /=
+// x += 5; 
+// (x = x + 5);
+
+//Unary operators: ++, --
+
+// for(i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+//Control structures and loops
+//conditional statements: if, else, if-else-if, switch 
+
+// let temperature = 25;
+
+// if (temperature < 0) {
+//     console.log("It's freezing");
+// } else if (temperature >= 0 || temperature < 20) {
+//     console.log("It's cool outside");
+// } else {
+//     console.log("It's a warm day");
+// }
+
+// switch (true) {
+//     case (temperature < 0):
+//         console.log("It's freezing");
+//         break;
+//     case (temperature >= 0 || temperature < 20):
+//         console.log("It's cool outside");
+//         break;
+//     default:
+//         console.log("It's a warm day");
+// }
+
+//loops: while loop, do-while
+// let count = 1;
+
+// while (count <= 5) {
+//     console.log(count);
+//     count++;
+// }
+
+// for(i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+let counter = 1;
+do {
+    console.log(counter);
+    counter++;
+} while (counter <= 5);
+
+
+
+
